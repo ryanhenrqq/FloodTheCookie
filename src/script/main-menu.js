b468c2f223b3025b28ef04a70117ae5f2296a98f
ryanhenrqq <ryanh8116@gmail.com>
2025-12-11 20:59:22 -0300

    test: vibration usages and availability test

--- src/script/main-menu.js
+++ src/script/main-menu.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
+    navigator.vibrate(200)
     returnNormalTitle()
 })
 function start(){
+    navigator.vibrate(200)
     window.location.href = "./pg/ingame.html"  
 }
 function continueGm() {
+    navigator.vibrate(200)
     const alerter = document.getElementById("alert-title")
     alerter.textContent = "Não há jogo salvo!"
     alerter.style.color = "red"
