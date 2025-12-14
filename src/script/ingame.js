7fa5f8171c9758a8f5b2249041b03659b9590d6a
ryanhenrqq <ryanh8116@gmail.com>
2025-12-13 21:42:42 -0300

    feat: added click visual effect of a falling cookie

--- src/script/ingame.js
+++ src/script/ingame.js
@@ -37,12 +37,35 @@ function uptimeSetter() {
     }
 }
 
+let activationMilisecs = 0
+let msx = 0
+let msy = 0
 document.addEventListener("mousedown", (e) =>{
-    const cookieFx = document.getElementById("effectMouse")
-    const msx = e.clientX
-    const msy = e.clientY
-    cookieFx.style.transform = `translate(${msx}px, ${msy}px)`
+    
+    if (activationMilisecs == 0) {
+        msx = e.clientX
+        msy = e.clientY
+        mouseEffectClick()
+    } else {
+        return
+    }
 })
+function mouseEffectClick() {
+    const cookieFx = document.getElementById("effectMouse")
+    cookieFx.style.display = "block"
+    cookieFx.style.transform = `translate(${msx}px, ${msy}px)`
+    if (activationMilisecs <= 15) {
+        msy = msy + 1
+        activationMilisecs = activationMilisecs + 1
+        setTimeout(mouseEffectClick, 10)
+    } else {
+        msx = 0
+        msy = 0
+        activationMilisecs = 0
+        cookieFx.style.display = "none"
+    }
+    
+}
 cookie.addEventListener("click", function(e){
     if (navigator.vibrate) {
         navigator.vibrate(50)
